6cb01ea99cc93973665fdb32d396bc1a14d054ee
LYH977 <ho0i97@hotmail.com>
2019-10-25 20:10:50 +0800

    forge 15

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          buy forge 15
+          buy forfdggge 15
         </p>
         <a
           className="App-link"
