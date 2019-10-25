612cd81e72a3598a15a5eb1eedd7764169115ef1
LYH977 <44613097+LYH977@users.noreply.github.com>
2019-10-25 19:03:56 +0800

    Procs
    haha

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wei Xien is a Pro.
+          PROCS.
         </p>
         <a
           className="App-link"
