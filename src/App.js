e10d15f31dbb46890252f979a8af86c86dc08213
yinwenn <limyinwen@hotmail.com>
2019-10-30 15:37:15 +0800

    bc

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bwx is a gay
+          weixien is a gay
         </p>
         <a
           className="App-link"
