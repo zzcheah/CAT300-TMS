648597b498de560e1ac49d81f1a1c85b1d8f7946
LYH977 <ho0i97@hotmail.com>
2019-11-04 18:56:07 +0800

    start doing React-Redux-Firebase (RRF)

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          john is a gay gay 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+      <h1>startiting to do react redux firebase</h1>
+
+
     </div>
   );
 }
