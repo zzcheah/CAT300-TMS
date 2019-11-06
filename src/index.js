a496f96b3efba29e8405c25783bafb74c66aaa25
LYH977 <ho0i97@hotmail.com>
2019-11-06 21:42:50 +0800

    part 17 firebase bug, finding solution

--- src/index.js
+++ src/index.js
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware, compose} from 'redux'
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
-import { getFirestore } from 'redux-firestore'
-import { getFirebase} from 'react-redux-firebase'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase} from 'react-redux-firebase'
+import firebase from './config/fbConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true,
+}
+
+const composeEnhancers = 
+        process.env.NODE_ENV === 'development'
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+        : compose;
+
+const store = createStore(
+    rootReducer, 
+    // composeEnhancers(
+    //     reactReduxFirebase(firebase, rrfConfig),
+    //     reduxFirestore(firebase),
+    //     applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+
+    // )
+   );
 
 
 
