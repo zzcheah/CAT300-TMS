a6ca8fe3f0204c1fe94fb72413be7d40f693cd97
zzcheah <zzcheah@users.noreply.github.com>
2019-11-19 18:41:58 +0800

    huuu

--- src/store/actions/trainingActions.js
+++ src/store/actions/trainingActions.js
@@ -1,3 +1,5 @@
+import firebase from "../../config/fbConfig";
+
 export const createTraining = training => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make asyn call to database
@@ -19,5 +21,30 @@ export const createTraining = training => {
       .catch(err => {
         dispatch({ type: "CREATE_TRAINING_ERROR", err });
       });
+  };
+};
+
+export const fetchOrganizers = () => {
+  return dispatch => {
+    firebase
+      .firestore()
+      .collection("organizers")
+      .get()
+      .then(snapshot => {
+        var organizers = [];
+        if (snapshot.docs.empty != false) {
+          snapshot.docs.forEach(doc => {
+            organizers.push(doc.data().name);
+          });
+          dispatch(setOrganizers(organizers));
+        }
+      });
+  };
+};
+
+export const setOrganizers = organizers => {
+  return {
+    type: "SET_ORGANIZERS",
+    payload: organizers
   };
 };
